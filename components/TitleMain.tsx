5d58e732cce8673469d873d8ffbf0c47eb00ee6c
unknown <diego.no.ramos@gmail.com>
2025-02-27 15:49:26 +0100

    Añadir el footer

--- components/TitleMain.tsx
+++ components/TitleMain.tsx
@@ -7,7 +7,7 @@ export const TitleMain = ({
     title
 }: TitleMainProp) => {
     return (
-        <h1 className="text-5xl font-bold text-center mb-12 text-jul-purple bg-white bg-opacity-0 p-4 rounded-lg">
+        <h1 className="text-4xl font-bold text-center mb-12 text-jul-purple bg-white bg-opacity-0 p-4 rounded-lg">
             {title}
         </h1>
     );
@@ -17,7 +17,7 @@ export const TitleMainSinmb = ({
     title
 }: TitleMainProp) => {
     return (
-        <h1 className="text-5xl font-bold text-center text-jul-purple bg-white bg-opacity-0 p-4 rounded-lg">
+        <h1 className="text-4xl font-bold text-center text-jul-purple bg-white bg-opacity-0 p-4 rounded-lg">
             {title}
         </h1>
     );
